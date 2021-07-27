f4959d0f3aa9689687b4b395fbeb3c380caa7ae6
Max <70536153+maxinocencio@users.noreply.github.com>
2021-07-27 20:31:12 -0300

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -16,27 +16,27 @@ client = commands.Bot(command_prefix = "_", case_insesitive = False)
 async def on_ready():
     print('~Online~')
 
-#comando para responder um comando no chat do discord
+#comando para responder um comando no chat do discord - Comando: _teste
 
 @client.command()
 async def teste(ctx):
     await ctx.send('Funciona')
 
-#comando somar 2 numeros
+#comando somar 2 numeros  - Comando: _soma 1 1 (os numeros a serem somados, separados por espaço)
 
 @client.command() 
 async def soma(ctx, *nums):
     operation = " + ".join(nums)
     await ctx.send(f'{operation} = {eval(operation)}')
 
-#sortear numeros
+#sortear numeros  - Comando: _sorteio 80 (Sorteia números de 1 à 80)
 
 @client.command()
 async def sorteio(ctx, numero):
     valor = random.randint(1,int(numero))
     await ctx.send(f'O número sorteado é {valor}')
 
-#trocar apelido - necessário criar um cargo com as permissões e atribuir ao bot
+#trocar apelido - necessário criar um cargo com as permissões e atribuir ao bot - Comando: _trocarapelido @user novoApelido
 
 @client.command(pass_content = True)
 async def trocarapelido(ctx, member: discord.Member, nick):
